f458ad867c87fe02b0767b12c8be94365fcd3e2f
rkm09 <raksha.mishra09@gmail.com>
2024-02-02 21:36:12 +0530

    edit distance

--- src/main/java/top150/dp/EditDistance72.java
+++ src/main/java/top150/dp/EditDistance72.java
@@ -8,8 +8,38 @@ public class EditDistance72 {
         System.out.println(minDistance(word1, word2));
     }
 
-//    top down dp with memoization; time: O(m.n), space: O(m.n)
     public static int minDistance(String word1, String word2) {
+        int word1Length = word1.length();
+        int word2Length = word2.length();
+        if(word1Length == 0)
+            return word2Length;
+        if(word2Length == 0)
+            return word1Length;
+
+        int[][] dp = new int[word1Length + 1][word2Length + 1];
+        for(int word1Index = 1 ; word1Index <= word1Length ; word1Index++){
+            dp[word1Index][0] = word1Index;
+        }
+        for(int word2Index = 1 ; word2Index <= word2Length ; word2Index++) {
+            dp[0][word2Index] = word2Index;
+        }
+        for(int word1Index = 1 ; word1Index <= word1Length ; word1Index++) {
+            for(int word2Index = 1 ; word2Index <= word2Length ; word2Index++) {
+                if(word1.charAt(word1Index - 1) == word2.charAt(word2Index - 1)) {
+                    dp[word1Index][word2Index] = dp[word1Index - 1][word2Index - 1];
+                } else {
+                    dp[word1Index][word2Index] = Math.min(
+                            dp[word1Index][word2Index - 1],
+                            Math.min(dp[word1Index - 1][word2Index], dp[word1Index - 1][word2Index - 1])
+                    ) + 1;
+                }
+            }
+        }
+        return dp[word1Length][word2Length];
+    }
+
+//    top down dp with memoization; time: O(m.n), space: O(m.n)
+    public static int minDistance1(String word1, String word2) {
         memo = new Integer[word1.length() + 1][word2.length() + 1];
         return minDistanceRecur(word1, word2, word1.length(), word2.length());
     }
