4f3336d84820d76811b1e4a31f8d828e7db2f191
rkm09 <raksha.mishra09@gmail.com>
2024-02-04 16:19:01 +0530

    min window

--- src/main/java/leetdaily/medium/MinWindow79.java
+++ src/main/java/leetdaily/medium/MinWindow79.java
@@ -1,6 +1,10 @@
 package leetdaily.medium;
 
+import common.Pair;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MinWindow79 {
@@ -9,8 +13,56 @@ public class MinWindow79 {
         System.out.println(minWindow(s, t));
     }
 
-//    sliding window; time: O(|s| + |t|), space: O(|s| + |t|)
+//    optimized sliding window; time: O(|s| + |t|), space: O(|s| + |t|)
+//  helpful when the length of t << length of s; only store the relevant chars of s
     public static String minWindow(String s, String t) {
+        if(s.isEmpty() || t.isEmpty()) return "";
+        Map<Character, Integer> targetCount = new HashMap<>();
+        for(char c : t.toCharArray()) {
+            targetCount.put(c, targetCount.getOrDefault(c, 0) + 1);
+        }
+        int required = targetCount.size();
+        List<Pair<Character, Integer>> filteredS = new ArrayList<>();
+        for(int i = 0 ; i < s.length() ; i++) {
+            char c = s.charAt(i);
+            if(targetCount.containsKey(c)) {
+                filteredS.add(new Pair<>(c, i));
+            }
+        }
+        Map<Character, Integer> windowCount = new HashMap<>();
+        int l = 0, r = 0, formed = 0;
+        int[] ans = {-1, 0, 0};
+        while(r < filteredS.size()) {
+            char c = filteredS.get(r).getKey();
+            windowCount.put(c, windowCount.getOrDefault(c, 0) + 1);
+            if(targetCount.containsKey(c) && windowCount.get(c).intValue() == targetCount.get(c).intValue()) {
+                formed++;
+            }
+//            keep reducing window size and update ans, as long as it is valid
+            while(l <= r && formed == required) {
+                char c1 = filteredS.get(l).getKey();
+                int start = filteredS.get(l).getValue();
+                int end = filteredS.get(r).getValue();
+//                save the smallest window so far
+                if(ans[0] == -1 || end - start + 1 < ans[0]) {
+                    ans[0] = end - start + 1;
+                    ans[1] = start;
+                    ans[2] = end;
+                }
+                windowCount.put(c1, windowCount.get(c1) - 1);
+                if(windowCount.get(c1) < targetCount.get(c1)) {
+                   formed--;
+                }
+                l++;
+            }
+            r++;
+        }
+
+        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
+    }
+
+//    sliding window; time: O(|s| + |t|), space: O(|s| + |t|)
+    public static String minWindow1(String s, String t) {
         if (s.isEmpty() || t.isEmpty()) return "";
         Map<Character, Integer> targetCount = new HashMap<>();
         for (char c : t.toCharArray()) {
