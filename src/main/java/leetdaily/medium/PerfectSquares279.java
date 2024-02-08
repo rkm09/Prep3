27ddb3e12b6104a64c7cd20f71633728c2134df8
rkm09 <raksha.mishra09@gmail.com>
2024-02-08 12:22:21 +0530

    combinations

--- src/main/java/leetdaily/medium/PerfectSquares279.java
+++ src/main/java/leetdaily/medium/PerfectSquares279.java
@@ -1,14 +1,31 @@
 package leetdaily.medium;
 
+import java.util.Arrays;
+
 public class PerfectSquares279 {
     public static void main(String[] args) {
         System.out.println(numSquares(12));
     }
+
+//    dp (knapsack); time: O(n.sqrt(n)), space: O(n)
     public static int numSquares(int n) {
-        return 0;
-    }
-    private static boolean isSquare(int num) {
-        return true;
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        int maxSquareIndex = (int) Math.sqrt(n) + 1;
+        int[] squareNums = new int[maxSquareIndex];
+        for(int i = 1 ; i < maxSquareIndex ; i++) {
+            squareNums[i] = i * i;
+        }
+        for(int i = 1 ; i <= n ; i++) {
+            for(int s = 1; s < maxSquareIndex ; s++) {
+                if(i < squareNums[s])
+                    break;
+                dp[i] = Math.min(dp[i], dp[i - squareNums[s]] + 1);
+            }
+        }
+        return dp[n];
     }
 }
 
