cbf388227b40de2d71a667d9faba82d0e5f4daf8
rkm09 <raksha.mishra09@gmail.com>
2024-02-12 14:56:50 +0530

    find and replace

--- src/main/java/leetdaily/medium/FindAndReplaceString833.java
+++ src/main/java/leetdaily/medium/FindAndReplaceString833.java
@@ -1,6 +1,7 @@
 package leetdaily.medium;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class FindAndReplaceString833 {
@@ -9,11 +10,16 @@ public class FindAndReplaceString833 {
         int[] indices = {0,2};
         String[] sources = {"a", "cd"};
         String[] targets = {"eee","ffff"};
-        System.out.println(findReplaceString(s, indices, sources, targets));
+        FindAndReplaceString833 find = new FindAndReplaceString833();
+        System.out.println(find.findReplaceString(s, indices, sources, targets));
+        Replacement replace1 = new Replacement(0, "num", "string");
+        Replacement replace2 = new Replacement(4, "foo", "bar");
+        String text = "num foo";
+        System.out.println(find.replaceText(text, List.of(replace1, replace2)));
     }
 
 //    hashmap (piece table); time: O(n), space: O(n)
-    public static String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
+    public String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
         Map<Integer, Integer> lookup = new HashMap<>();
         for(int i = 0 ; i < indices.length ; i++) {
             if(s.startsWith(sources[i], indices[i])) {
@@ -33,6 +39,42 @@ public class FindAndReplaceString833 {
         return sb.toString();
     }
 
+    public String findReplaceString1(String s, int[] indices, String[] sources, String[] targets) {
+        return "";
+    }
+
+//    hashmap; time: O(n), space: O(n)
+    public String replaceText(String text, List<Replacement> ranges) {
+        Map<Integer, Integer> lookup = new HashMap<>();
+        for(int i = 0 ; i < ranges.size() ; i++) {
+//            assuming the string exists at the index, else use starts with
+            if(text.startsWith(ranges.get(i).before, ranges.get(i).start)) {
+                lookup.put(ranges.get(i).start, i);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < text.length() ;) {
+            if(lookup.containsKey(i)) {
+                sb.append(ranges.get(lookup.get(i)).after);
+                i += ranges.get(lookup.get(i)).before.length();
+            } else {
+                sb.append(text.charAt(i));
+                i++;
+            }
+        }
+        return sb.toString();
+    }
+
+}
+class Replacement {
+    int start;
+    String before;
+    String after;
+    Replacement(int start, String before, String after) {
+        this.start = start;
+        this.before = before;
+        this.after = after;
+    }
 }
 
 /*
