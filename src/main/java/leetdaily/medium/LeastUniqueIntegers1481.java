53f8725e681e9ca35977e429910cedd9f511b89d
rkm09 <raksha.mishra09@gmail.com>
2024-02-16 22:26:28 +0530

    least unique

--- src/main/java/leetdaily/medium/LeastUniqueIntegers1481.java
+++ src/main/java/leetdaily/medium/LeastUniqueIntegers1481.java
@@ -8,19 +8,80 @@ public class LeastUniqueIntegers1481 {
         int[] arr = {5,5,4};
         System.out.println(findLeastNumOfUniqueInts(arr, 1));
     }
+
+//    counting sort; time: O(n), space: O(n)
     public static int findLeastNumOfUniqueInts(int[] arr, int k) {
-        int ans = 0;
+        Map<Integer, Integer> countMap  = new HashMap<>();
+        for(int num : arr)
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        int n = arr.length;
+        int[] freq = new int[n + 1];
+        for(int count : countMap.values()) {
+            freq[count]++;
+        }
+        int remainingUniqueElements = countMap.size();
+        for(int i = 1 ; i <= n ; i++) {
+//            k/i represents the maximum possible elements that can be removed
+            int numElementsToRemove = Math.min(k / i, freq[i]);
+            k -= (i * numElementsToRemove);
+            remainingUniqueElements -= numElementsToRemove;
+            if(k < i) {
+                return remainingUniqueElements;
+            }
+        }
+        return 0;
+    }
+
+    //    hashmap; time: O(nlogn), space: O(n)
+    public static int findLeastNumOfUniqueInts2(int[] arr, int k) {
+        Map<Integer, Integer> count  = new HashMap<>();
+        for(int num : arr)
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        List<Integer> frequencies = new ArrayList<>(count.values());
+        Collections.sort(frequencies);
+        int elementsRemoved = 0;
+        for(int i = 0 ; i < frequencies.size() ; i++) {
+            elementsRemoved += frequencies.get(i);
+            if(elementsRemoved > k)
+                return frequencies.size() - i;
+        }
+//        exhausted all
+        return 0;
+    }
+
+    //     min heap; time: O(n), space: O(n)
+    public static int findLeastNumOfUniqueInts1(int[] arr, int k) {
+        Map<Integer, Integer> count  = new HashMap<>();
+        for(int num : arr)
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(count.values());
+        int elementsRemoved = 0;
+        while(!minHeap.isEmpty()) {
+            elementsRemoved += minHeap.peek();
+            if(elementsRemoved > k)
+                return minHeap.size();
+            minHeap.poll();
+        }
+        return 0;
+    }
+
+
+
+//    [def]; hashmap; time: O(nlogn), space: O(n)
+    public static int findLeastNumOfUniqueInts3(int[] arr, int k) {
         Map<Integer, Integer> count = new HashMap<>();
         for(int num : arr)
             count.put(num, count.getOrDefault(num, 0) + 1);
         Map<Integer, Integer> sorted = count.entrySet().stream().sorted(Map.Entry.comparingByValue())
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1,e2)->e1, LinkedHashMap::new));
-        int sum = 0;
+        int ans = 0, sum = 0, exclude = 0;
         for(int key : sorted.keySet()) {
             int val = sorted.get(key);
             sum += val;
-            if(sum <= k) continue;
-            ans++;
+            if(sum <= k) {
+                exclude++; continue;
+            }
+            return sorted.size() - exclude;
         }
         return ans;
     }
