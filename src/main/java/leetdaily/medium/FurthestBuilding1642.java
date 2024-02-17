5e327213282b2834f8979c23d6ad6873876f2032
rkm09 <raksha.mishra09@gmail.com>
2024-02-17 22:09:10 +0530

    furthest building

--- src/main/java/leetdaily/medium/FurthestBuilding1642.java
+++ src/main/java/leetdaily/medium/FurthestBuilding1642.java
@@ -1,7 +1,8 @@
 package leetdaily.medium;
 
-import java.util.PriorityQueue;
-import java.util.Queue;
+import common.Pair;
+
+import java.util.*;
 
 public class FurthestBuilding1642 {
     public static void main(String[] args) {
@@ -12,7 +13,7 @@ public class FurthestBuilding1642 {
         System.out.println(furthestBuilding(heights, 5, 1));
     }
 
-//  min heap (allocate minimum diff for bricks); time: O(nlogn), space: O(n)
+//  Min heap (allocate minimum diff for bricks); time: O(nlogn), space: O(n)
     public static int furthestBuilding(int[] heights, int bricks, int ladders) {
         Queue<Integer> ladderAllocations = new PriorityQueue<>();
         for(int i = 0 ; i < heights.length - 1 ; i++) {
@@ -32,10 +33,10 @@ public class FurthestBuilding1642 {
         return heights.length - 1;
     }
 
-    //  max heap (allocate maximum diff for ladders); time: O(nlogn), space: O(n)
+    //  Max heap (allocate maximum diff for ladders); time: O(nlogn), space: O(n)
     public static int furthestBuilding1(int[] heights, int bricks, int ladders) {
         Queue<Integer> brickAllocations = new PriorityQueue<>((a, b) -> b - a);
-        for(int i = 0 ; i < heights.length ; i++) {
+        for(int i = 0 ; i < heights.length - 1 ; i++) {
             int climb = heights[i + 1] - heights[i];
 //            jump down
             if(climb <= 0)
@@ -53,6 +54,102 @@ public class FurthestBuilding1642 {
         }
         return heights.length - 1;
     }
+
+    /* Binary search usage:
+        Given an input, determining if it is possible to go all the way across is an easy problem;
+        you could simply make a list of all the climbs, min-sort it, and then cover as many climbs as you can with bricks, and then cover the rest with ladders.
+        If this covered the entire climb list, then you’d know that it was possible to get all the way across.
+
+        On an odd-length search space, identifying the midpoint is straightforward.
+        On even-length search spaces, though, we have two possible midpoints.
+        The final step of the binary search algorithm design process is to decide whether it is the lower or higher midpoint that should be used.
+
+        It's generally easiest to set lo to represent the lowest possible position of the target and hi to be the highest possible position.
+
+        The short rule to remember is:
+        if you used hi = mid - 1, then use the higher midpoint.
+        If you used lo = mid + 1, then use the lower midpoint.
+        If you used both of these, then you can use either midpoint. If you didn’t use either (i.e., you have lo = mid and hi = mid), then, unfortunately, your code is buggy, and you won’t be able to guarantee convergence.
+     */
+
+//    binary search for final reachable building; time: O(nlog^2n) [logn for binary search with nlogn sorting each time], space: O(n)
+    public static int furthestBuilding2(int[] heights, int bricks, int ladders) {
+        int low = 0;
+        int high = heights.length - 1;
+        while(low < high) {
+            int mid = low + (high - low + 1) / 2;
+            if(isReachable2(mid, heights, bricks, ladders)) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+//        can return either lo or hi
+        return low;
+    }
+    private static boolean isReachable2(int buildingIndex, int[] heights, int bricks, int ladders) {
+        List<Integer> climbs = new ArrayList<>();
+        for(int i = 0 ; i < buildingIndex ; i++) {
+            int h1 = heights[i];
+            int h2 = heights[i + 1];
+            if(h1 > h2)
+                continue;
+            climbs.add(h2 - h1);
+        }
+
+        Collections.sort(climbs);
+
+        for(int climb : climbs) {
+            if(climb <= bricks) {
+                bricks -= climb;
+            } else if(ladders > 0){
+                ladders--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+//    Improved binary search; time: O(nlogn), space: O(n)
+    public static int furthestBuilding3(int[] heights, int bricks, int ladders) {
+        List<int[]> sortedClimbs = new ArrayList<>();
+        for(int i = 0 ; i < heights.length - 1; i++) {
+            int climb = heights[i + 1] - heights[i];
+            if(climb < 0)
+                continue;
+            sortedClimbs.add(new int[]{climb, i + 1});
+        }
+        Collections.sort(sortedClimbs, Comparator.comparingInt(a -> a[0]));
+
+        int low = 0;
+        int high = heights.length - 1;
+        while(low < high) {
+            int mid = low + (high - low + 1) / 2;
+            if(isReachable(mid, sortedClimbs, bricks, ladders)) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+    private static boolean isReachable(int buildingIndex, List<int[]> climbs, int bricks, int ladders) {
+        for(int[] climbEntry : climbs) {
+            int climb = climbEntry[0];
+            int index = climbEntry[1];
+            if(index > buildingIndex)
+                continue;
+            if(climb <= bricks)
+                bricks -= climb;
+            else if(ladders > 0)
+                ladders--;
+            else
+                return false;
+        }
+        return true;
+    }
+
 }
 
 /*
