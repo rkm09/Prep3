2267663a60c18866492b33d6dc3ac2b89ea920e6
rkm09 <raksha.mishra09@gmail.com>
2024-03-06 15:16:03 +0530

    meeting rooms

--- src/main/java/leetdaily/medium/MeetingRoomsII253.java
+++ src/main/java/leetdaily/medium/MeetingRoomsII253.java
@@ -1,25 +1,52 @@
 package leetdaily.medium;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class MeetingRoomsII253 {
     public static void main(String[] args) {
         int[][] intervals = {{0,30},{5,10},{15,20}};
         System.out.println(minMeetingRooms(intervals));
     }
+
+//    Priority Queue; time: O(nlogn), space: O(n)
     public static int minMeetingRooms(int[][] intervals) {
-        if (intervals.length == 1) return 1;
-        int rooms = 1;
-        for (int i = 0; i < intervals.length; i++) {
-            for (int j = i + 1; j < intervals.length; j++) {
-                if (overlap(intervals[i], intervals[i + 1])) {
-                    rooms++; break;
-                }
+        if(intervals.length == 0) return 0;
+        PriorityQueue<Integer> allocator = new PriorityQueue<>();
+        Arrays.sort(intervals, Comparator.comparingInt(a->a[0]));
+        allocator.offer(intervals[0][1]);
+        for(int i = 1 ; i < intervals.length ; i++) {
+            if(intervals[i][0] >= allocator.peek()) {
+                allocator.poll();
             }
+            allocator.offer(intervals[i][1]);
         }
-        return rooms;
+        return allocator.size();
     }
-    private static boolean overlap(int[] interval1, int[] interval2) {
-        return Math.min(interval1[1], interval2[1]) >
-                Math.max(interval1[0], interval2[0]);
+
+//    chronological ordering; time: O(nlogn), space: O(n) [faster]
+    public static int minMeetingRooms1(int[][] intervals) {
+        int n = intervals.length;
+        if(n == 0) return 0;
+        int[] start = new int[n];
+        int[] end = new int[n];
+        for(int i = 0 ; i < n ; i++) {
+            start[i] = intervals[i][0];
+            end[i] = intervals[i][1];
+        }
+        Arrays.sort(start);
+        Arrays.sort(end);
+        int usedRooms = 0, startPointer = 0, endPointer = 0;
+        while(startPointer < n) {
+            if(start[startPointer] >= end[endPointer]) {
+                usedRooms--;
+                endPointer++;
+            }
+            usedRooms++;
+            startPointer++;
+        }
+        return usedRooms;
     }
 }
 
