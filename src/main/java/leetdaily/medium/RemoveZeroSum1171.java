366708805975e256ae1ec5f7c43c3f41a6f3711c
rkm09 <raksha.mishra09@gmail.com>
2024-03-14 10:28:54 +0530

    remove zero sum

--- src/main/java/leetdaily/medium/RemoveZeroSum1171.java
+++ src/main/java/leetdaily/medium/RemoveZeroSum1171.java
@@ -12,20 +12,20 @@ public class RemoveZeroSum1171 {
         ListNode next1 = new ListNode(3, next2);
         ListNode next = new ListNode(-3, next1);
         ListNode head = new ListNode(1, next);
-        ListNode result = removeZeroSumSublists(head);
+        ListNode result = removeZeroSumSublists1(head);
         while(result != null) {
             System.out.println(result.val);
             result = result.next;
         }
     }
 
-//    prefix sum & hashmap; time: O(n), space: O(n)
+//    prefix sum & hashmap (2 pass); time: O(n), space: O(n)
     public static ListNode removeZeroSumSublists(ListNode head) {
         ListNode front = new ListNode(0, head);
         ListNode current = front;
         int prefixSum = 0;
         Map<Integer, ListNode> prefixSumToNode = new HashMap<>();
-        prefixSumToNode.put(0, front);
+//        prefixSumToNode.put(0, front);
 //      1st pass: calculate prefix sum
         while(current != null) {
             prefixSum += current.val;;
@@ -44,8 +44,37 @@ public class RemoveZeroSum1171 {
         return front.next;
     }
 
-//    prefix sum; time: O(n^2), space: O(1)
+    //    prefix sum & hashmap (1 pass); time: O(n), space: O(n)
     public static ListNode removeZeroSumSublists1(ListNode head) {
+        ListNode front = new ListNode(0, head);
+        ListNode current = front;
+        int prefixSum = 0;
+        Map<Integer, ListNode> prefixSumToNode = new HashMap<>();
+        while(current != null) {
+            prefixSum += current.val;
+//            if prefix sum is already in the hashmap zero sum sequence has been found;
+            if(prefixSumToNode.containsKey(prefixSum)) {
+                ListNode prev = prefixSumToNode.get(prefixSum);
+                current = prev.next;
+                int p = current.val + prefixSum;
+//                delete zero sum nodes from the hashmap
+                while(p != prefixSum) {
+                    prefixSumToNode.remove(p);
+                    current = current.next;
+                    p += current.val;
+                }
+//                make the connection from node before zero sum to the node after
+                prev.next = current.next;
+            } else {
+                prefixSumToNode.put(prefixSum, current);
+            }
+            current = current.next;
+        }
+        return front.next;
+    }
+
+//    prefix sum; time: O(n^2), space: O(1)
+    public static ListNode removeZeroSumSublists2(ListNode head) {
         ListNode front = new ListNode(0, head);
         ListNode start = front;
         while(start != null) {
