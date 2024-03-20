ec36362867a076b9f9eb564e52ec1949eb690995
rkm09 <raksha.mishra09@gmail.com>
2024-03-20 11:37:22 +0530

    task scheduler

--- src/main/java/leetdaily/medium/TaskScheduler621.java
+++ src/main/java/leetdaily/medium/TaskScheduler621.java
@@ -5,8 +5,8 @@ import java.util.*;
 
 public class TaskScheduler621 {
     public static void main(String[] args) {
-        char[] tasks = {'A','A','B','C'};
-        System.out.println(leastInterval(tasks, 2));
+        char[] tasks = {'A','A','A','B','B','B','C','C','C'};
+        System.out.println(leastInterval1(tasks, 2));
     }
 
 //    all approaches use greedy => decisions are made step by step based on what seems best at the moment to reach an overall optimal
@@ -20,17 +20,59 @@ public class TaskScheduler621 {
         Arrays.sort(freq);
 
 //       max interval length
-        int maxIdleFreq = freq[25] - 1;
-        int idleSlots = maxIdleFreq * n;
+        int maxIdleInterval = freq[25] - 1;
+        int idleSlots = maxIdleInterval * n;
         for(int i = 24 ; i >= 0 && freq[i] > 0; i--) {
-            idleSlots -= Math.min(maxIdleFreq, freq[i]);
+            idleSlots -= Math.min(maxIdleInterval, freq[i]);
         }
         return idleSlots > 0 ? idleSlots + tasks.length : tasks.length;
     }
 
+//    using math formula; time: O(n), space: O(n)
+    public static int leastInterval1(char[] tasks, int n) {
+        int[] freq = new int[26];
+        int maxCount = 0;
+        for(char task : tasks) {
+            freq[task - 'A']++;
+            maxCount = Math.max(maxCount, freq[task - 'A']);
+        }
+//        all except the last one at max freq will not require idles; n + 1 => 1 for the task itself & n idles
+        int time = (maxCount - 1) * (n + 1);
+//        add one each for the last count eg. calculated already [A--A--] [A] <-last one for all at that count
+        for(int fr : freq) {
+            if(fr == maxCount) time++;
+        }
+        return Math.max(tasks.length, time);
+    }
+
+//    greedy; time: O(n), space: O(n)
+    public static int leastInterval2(char[] tasks, int n) {
+        int[] counter = new int[26];
+        int maximum = 0, maxCount = 0;
+        for(char task : tasks) {
+            counter[task - 'A']++;
+            if(maximum == counter[task - 'A']) {
+                maxCount++;
+            } else if(maximum < counter[task - 'A']) {
+                maxCount = 1;
+                maximum = counter[task - 'A'];
+            }
+        }
+//        partLength & partCount deal with the max freq elements exclusively (eliminate these from the empty slot count)
+        int partCount = maximum - 1;
+        int partLength = n - (maxCount - 1);
+        int emptySlots = partCount * partLength;
+//        calculate with the remaining
+        int availableTasks = tasks.length - maxCount * maximum;
+        int idles = Math.max(0, emptySlots - availableTasks);
+
+        return tasks.length + idles;
+
+    }
+
 //    choosing tasks with max frequency minimizes overall idle time
 //    priority queue; time: O(n), space: O(1) [pq operations take O(logk), so overall O(nlogk); but since k = 26 (constant); hence O(n)]
-    public static int leastInterval1(char[] tasks, int n) {
+    public static int leastInterval3(char[] tasks, int n) {
         int[] freq = new int[26];
         for(char c : tasks)
             freq[c - 'A']++;
