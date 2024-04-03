f2f914edf04933742f9bcff45f064a97b5ef29f1
rkm09 <raksha.mishra09@gmail.com>
2024-04-03 14:03:48 +0530

    word search

--- src/main/java/top150/backtracking/WordSearch79.java
+++ src/main/java/top150/backtracking/WordSearch79.java
@@ -24,19 +24,25 @@ public class WordSearch79 {
         return false;
     }
 
-//    backtracking; time: O(N.3^L), space: O(N) [N - number of cells in the board, L - length of the word to be matched]
+//    backtracking; time: O(N.3^L), space: O(L) [N - number of cells in the board, L - length of the word to be matched]
+//    For the backtracking function, initially we could have at most 4 directions to explore, but further the choices are reduced into 3 (since we won't go back to where we come from).
+//    As a result, the execution trace after the first step could be visualized as a 3-ary tree, each of the branches represent a potential exploration in the corresponding direction.
+//    Therefore, in the worst case, the total number of invocation would be the number of nodes in a full 3-nary tree, which is about 3^L.
+//    We iterate through the board for backtracking, i.e. there could be N times invocation for the backtracking function in the worst case.
     private boolean backtrack(int row, int col, String word, int index) {
 //      check base case
         if(index >= word.length())
             return true;
-//      check boundaries
+//      check boundaries & value
         if(row < 0 || row == ROWS || col < 0 || col == COLS || board[row][col] != word.charAt(index))
             return false;
 //      else mark the path before the next exploration
         board[row][col] = '#';
         boolean isValidPath = false;
+//        right, down, up, left
         int[] rowOffset = {0, 1, 0, -1};
         int[] colOffset = {1, 0, -1, 0};
+//        check in all four directions with offsets
         for(int d = 0 ; d < 4 ; d++) {
             isValidPath = backtrack(row + rowOffset[d], col + colOffset[d], word, index + 1);
             if(isValidPath) break;
