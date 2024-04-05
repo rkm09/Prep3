076a8d823c530557db59358774bb884527f97d54
rkm09 <raksha.mishra09@gmail.com>
2024-04-05 22:45:15 +0530

    trapping water

--- src/main/java/top150/strings_arrays/TrappingRainWater42.java
+++ src/main/java/top150/strings_arrays/TrappingRainWater42.java
@@ -6,8 +6,29 @@ public class TrappingRainWater42 {
         System.out.println(trap(heights));
     }
 
-//    brute force; time: O(n^2), space: O(1)
+
+//    In brute force, we iterate over the left and right parts again and again just to find the highest bar size until that index.
+//    But, this could be stored. => DP
     public static int trap(int[] height) {
+        int n = height.length, ans = 0;
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+        for(int i = 1 ; i < n ; i++) {
+          leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for(int i = n - 2 ; i >= 0 ; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+        for(int i = 1 ; i < n - 1 ; i++) {
+            ans += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return ans;
+    }
+
+//    brute force; time: O(n^2), space: O(1)
+    public static int trap1(int[] height) {
         int n = height.length, ans = 0;
 //        note the first and the last need not be considered when standing at the 1st and last respectively, as there is no wall beyond them
         for(int i = 1 ; i < n - 1 ; i++) {
