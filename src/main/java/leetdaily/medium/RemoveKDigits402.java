0a76c24cd2c5b8bfdb70eb0589fbd78206359381
rkm09 <raksha.mishra09@gmail.com>
2024-04-11 11:44:47 +0530

    remove k

---
+++ src/main/java/leetdaily/medium/RemoveKDigits402.java
@@ -0,0 +1,61 @@
+package leetdaily.medium;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class RemoveKDigits402 {
+    public static void main(String[] args) {
+        String num = "1432219";
+        System.out.println(removeKdigits(num, 3));
+    }
+
+//    given two number sequences, it is the leftmost 'distinct' digit which determines the superior of the two. 1axx, 1bxx
+//    greedy; time: O(n), space: O(n)
+    public static String removeKdigits(String num, int k) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for(char c : num.toCharArray()) {
+            while(!stack.isEmpty() && k > 0 && stack.peek() > c) {
+                stack.pop();
+                k--;
+            }
+            stack.push(c);
+        }
+        while(k > 0 && !stack.isEmpty()) {
+            stack.removeLast();
+            k--;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        boolean isLeadingZero = true;
+        while(!stack.isEmpty()) {
+            char c = stack.removeLast();
+            if(isLeadingZero &&  c == '0') continue;
+            isLeadingZero =  !isLeadingZero;
+            sb.append(c);
+        }
+
+        if(sb.length() == 0) return "0";
+        return sb.toString();
+    }
+}
+
+/*
+Given string num representing a non-negative integer num, and an integer k, return the smallest possible integer after removing k digits from num.
+Example 1:
+Input: num = "1432219", k = 3
+Output: "1219"
+Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+Example 2:
+Input: num = "10200", k = 1
+Output: "200"
+Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+Example 3:
+Input: num = "10", k = 2
+Output: "0"
+Explanation: Remove all the digits from the number and it is left with nothing which is 0.
+
+Constraints:
+1 <= k <= num.length <= 105
+num consists of only digits.
+num does not have any leading zeros except for the zero itself.
+ */
