6545717905e4535f61d8d3b16eb271c6ce5003a4
rkm09 <raksha.mishra09@gmail.com>
2024-04-19 11:28:47 +0530

    islands

--- src/main/java/leetdaily/medium/NumberOfIslands200.java
+++ src/main/java/leetdaily/medium/NumberOfIslands200.java
@@ -14,7 +14,35 @@ public class NumberOfIslands200 {
 
 //    dfs; time: O(m*n), space: O(m*n)
     public int numIslands(char[][] grid) {
-        
+        if(grid == null || grid.length == 0) return 0;
+        int nr = grid.length, nc = grid[0].length;
+        int numOfIslands = 0;
+        for(int r = 0 ; r < nr ; r++) {
+            for(int c = 0 ; c < nc ; c++) {
+                if(grid[r][c] == '1') {
+                    numOfIslands++;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+        return numOfIslands;
+    }
+    private void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length, nc = grid[0].length;
+        if(r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0')
+            return;
+//        mark visited
+        grid[r][c] = '0';
+//        explore all four directions
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+
+//    bfs; time: O(m*n), space: O(m*n)
+    public int numIslands1(char[][] grid) {
+
     }
 
 //    disjoint set union; time: O(m*n), space: O(m*n) [considering dsu operations to be constant time with ackermann fn]
@@ -25,6 +53,8 @@ public class NumberOfIslands200 {
           for(int r = 0 ; r < nr ; r++) {
               for(int c = 0 ; c < nc ; c++) {
                   if(grid[r][c] == '1') {
+//                      mark visited
+                        grid[r][c] = '0';
                         if(r - 1 >= 0 && grid[r - 1][c] == '1') {
                             dsu.union(r * nc + c, (r - 1) * nc + c);
                         }
