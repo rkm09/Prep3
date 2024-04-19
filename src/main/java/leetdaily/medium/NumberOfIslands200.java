fdc9ab280d9b1d304f160326a15d1e4052cc8493
rkm09 <raksha.mishra09@gmail.com>
2024-04-19 11:58:37 +0530

    islands

--- src/main/java/leetdaily/medium/NumberOfIslands200.java
+++ src/main/java/leetdaily/medium/NumberOfIslands200.java
@@ -1,5 +1,8 @@
 package leetdaily.medium;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class NumberOfIslands200 {
     public static void main(String[] args) {
         char[][] grid = {
@@ -12,7 +15,7 @@ public class NumberOfIslands200 {
         System.out.println(n.numIslands(grid));
     }
 
-//    dfs; time: O(m*n), space: O(m*n)
+//    dfs; time: O(m*n), space: O(m*n) [fastest]
     public int numIslands(char[][] grid) {
         if(grid == null || grid.length == 0) return 0;
         int nr = grid.length, nc = grid[0].length;
@@ -42,7 +45,43 @@ public class NumberOfIslands200 {
 
 //    bfs; time: O(m*n), space: O(m*n)
     public int numIslands1(char[][] grid) {
-
+        if(grid == null || grid.length == 0) return 0;
+        int nr = grid.length, nc = grid[0].length;
+        int numOfIslands = 0;
+        for(int r = 0 ; r < nr ; r++) {
+            for(int c = 0 ; c < nc ; c++) {
+                if(grid[r][c] == '1') {
+                    numOfIslands++;
+//                    mark visited
+                    grid[r][c] = '0';
+                    Deque<Integer> queue = new ArrayDeque<>();
+                    queue.offer(r * nc + c);
+                    while(!queue.isEmpty()) {
+                        int cell = queue.poll();
+                        int row = cell / nc;
+                        int col = cell % nc;
+//                        mark visited
+                        if(row - 1 >= 0 && grid[row - 1][col] == '1') {
+                            queue.offer((row - 1) * nc + col);
+                            grid[row - 1][col] = '0';
+                        }
+                        if(row + 1 < nr && grid[row + 1][col] == '1') {
+                            queue.offer((row + 1) * nc + col);
+                            grid[row + 1][col] = '0';
+                        }
+                        if(col - 1 >= 0 && grid[row][col - 1] == '1') {
+                            queue.offer(row * nc + (col - 1));
+                            grid[row][col - 1] = '0';
+                        }
+                        if(col + 1 < nc && grid[row][col + 1] == '1') {
+                            queue.offer(row * nc + (col + 1));
+                            grid[row][col + 1] = '0';
+                        }
+                    }
+                }
+            }
+        }
+        return numOfIslands;
     }
 
 //    disjoint set union; time: O(m*n), space: O(m*n) [considering dsu operations to be constant time with ackermann fn]
