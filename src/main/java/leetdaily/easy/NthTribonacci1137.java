65a5dbd5d0c1edcf5f807584c5c6d7e728dcc8a1
rkm09 <raksha.mishra09@gmail.com>
2024-04-24 06:05:56 +0530

    tribonacci

---
+++ src/main/java/leetdaily/easy/NthTribonacci1137.java
@@ -0,0 +1,43 @@
+package leetdaily.easy;
+
+import java.util.Arrays;
+
+public class NthTribonacci1137 {
+    private static int[] cache;
+    public static void main(String[] args) {
+        System.out.println(tribonacci(4));
+    }
+    public static int tribonacci(int n) {
+        if(n < 2) return n;
+        cache = new int[n + 1];
+        Arrays.fill(cache, -1);
+        cache[0] = 0;
+        cache[1] = 1;
+        cache[2] = 1;
+        return helper(n);
+    }
+    private static int helper(int n) {
+        if(cache[n] != -1) return cache[n];
+        cache[n] = helper(n - 1) + helper(n - 2) + helper(n - 3);
+        return cache[n];
+    }
+}
+
+/*
+The Tribonacci sequence Tn is defined as follows:
+T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+Given n, return the value of Tn.
+Example 1:
+Input: n = 4
+Output: 4
+Explanation:
+T_3 = 0 + 1 + 1 = 2
+T_4 = 1 + 1 + 2 = 4
+Example 2:
+Input: n = 25
+Output: 1389537
+
+Constraints:
+0 <= n <= 37
+The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 - 1.
+ */
