845432f5fa2187ceeec09bc3376b0a5e9604eb80
rkm09 <raksha.mishra09@gmail.com>
2024-05-22 16:45:00 +0530

    palindrome

--- src/main/java/leetdaily/medium/PalindromePartitioning131.java
+++ src/main/java/leetdaily/medium/PalindromePartitioning131.java
@@ -6,14 +6,15 @@ import java.util.List;
 public class PalindromePartitioning131 {
     private List<List<String>> result;
     private int n;
+    private boolean[][] dp;
     public static void main(String[] args) {
         PalindromePartitioning131 p = new PalindromePartitioning131();
-        String s = "aab";
+        String s = "aaa";
         List<List<String>> res = p.partition(s);
         res.forEach(System.out::println);
     }
 
-//    backtracking; time: O(N*2^N), space: O(N)
+//    backtracking; time: O(N*2^N), space: O(N) [N is the length of the string s, 2^N is the possible number of substrings]
     public List<List<String>> partition(String s) {
         result = new ArrayList<>();
         n = s.length();
@@ -42,6 +43,35 @@ public class PalindromePartitioning131 {
                 return false;
         }
         return true;
+    }
+
+//    dp; time: O(N*2^N), space: O(N*N)
+//    the previous approach performs one extra iteration to determine if a given substring is a palindrome or not.
+//    we are repeatedly iterating over the same substring multiple times and the result is always the same.
+//    there are overlapping sub problems which could further be optimized with dp;
+    public List<List<String>> partition1(String s) {
+        n = s.length();
+        result = new ArrayList<>();
+        dp = new boolean[n][n];
+        backtrackDp(0, new ArrayList<>(), s);
+        return result;
+    }
+
+    private void backtrackDp(int start, List<String> curr, String s) {
+        if(start == n) {
+            result.add(new ArrayList<>(curr));
+            return;
+        }
+        for(int end = start ; end < n ; end++) {
+            if(s.charAt(start) == s.charAt(end) &&
+                    (end - start <= 2 || dp[start + 1][end - 1])
+            ) {
+                dp[start][end] = true;
+                curr.add(s.substring(start, end + 1));
+                backtrackDp(end + 1, curr, s);
+                curr.remove(curr.size() - 1);
+            }
+        }
     }
 }
 
