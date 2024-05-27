560857d2588d4bb2a416f4ab0bbb051a06e9ea80
rkm09 <raksha.mishra09@gmail.com>
2024-05-27 11:47:24 +0530

    special array

--- src/main/java/leetdaily/easy/SpecialArray1608.java
+++ src/main/java/leetdaily/easy/SpecialArray1608.java
@@ -36,6 +36,22 @@ public class SpecialArray1608 {
         }
         return index;
     }
+
+//    counting sort; prefix sum; time: O(n), space: O(n)
+    public static int specialArray1(int[] nums) {
+        int n = nums.length;
+        int[] freq = new int[n + 1];
+        for(int i = 0 ; i < n ; i++) {
+            freq[Math.min(n, nums[i])]++;
+        }
+        int numGreaterThanOrEqual = 0;
+        for(int i = n ; i >= 1 ; i--) {
+            numGreaterThanOrEqual += freq[i];
+            if(numGreaterThanOrEqual == i)
+                return i;
+        }
+        return -1;
+    }
 }
 
 /*
